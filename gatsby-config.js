ce315a4d8041dcf8be4384b50a24a6a6e4117397
Alexander Mikhailov <alex.mikhailov@idt.net>
2021-09-16 08:29:04 -0400

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,37 @@
+module.exports = {
+    siteMetadata: {
+        siteUrl: `https://bosswireless.com`,
+        title: "BOSS Wireless",
+        description: `BOSS Wireless Description`,
+        author: `IDT`,
+      },
+      plugins: [
+        `gatsby-plugin-postcss`,  
+        {
+          resolve: `gatsby-source-filesystem`,
+          options: {
+            path: `${__dirname}/src/pages`,
+            name: `pages`,
+          },
+        },
+        `gatsby-plugin-react-helmet`,
+        `gatsby-plugin-netlify-cms`,
+        {
+          resolve: `gatsby-theme-i18n`,
+          options: {
+            defaultLang: `en-us`,
+            prefixDefault: true,
+            configPath: require.resolve(`./i18n/config.json`),
+          },
+        },
+        {
+          resolve: `gatsby-theme-i18n-react-i18next`,
+          options: {
+            locales: `./i18n/react-i18next`,
+            i18nextOptions: {
+              ns: ["translation"],
+            },
+          },
+        }
+      ]
+    }
